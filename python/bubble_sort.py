d34d34ded7ebb81ace3318c80530d7e2d629ed08
Sarah Gray <sgray@neat.com>
2013-06-09 21:31:33 -0400

    adding python bubble sort

---
+++ python/bubble_sort.py
@@ -0,0 +1,8 @@
+def bubble_sort(numbers):
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            if numbers[j] < numbers[i]:
+                swap = numbers[j]
+                numbers[j] = numbers[i]
+                numbers[i] = swap
+    return numbers
