e4283abcb7b7668a02583649bd5f2e76ab48fe35
Sarah Gray <sgray@neat.com>
2013-06-09 22:40:35 -0400

    adding sample call to python sequential search

--- python/sequential_search.py
+++ python/sequential_search.py
@@ -1,5 +1,10 @@
-def find_index(numbers, key):
+def sequential_search(numbers, key):
     for index in range(len(numbers)):
         if numbers[index] == key:
             return index #There it is!
     return -1 #Nope, not in the array
+
+
+result = sequential_search([1,2,3,4,5,23,56,78], 23)
+
+print "result is " + str(result)
