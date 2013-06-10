bdd092561fc98b96941f4d9923922ef1d0e83cf5
Sarah Gray <sgray@neat.com>
2013-06-09 22:41:43 -0400

    adding sample call to python bubble sort

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -6,3 +6,7 @@ def bubble_sort(numbers):
                 numbers[j] = numbers[i]
                 numbers[i] = swap
     return numbers
+
+result = bubble_sort([1, 2, 3, 4, 5, 23, 56, 78, 22, 45, 9])
+
+print "result is " + str(result)
